ec95d215039f4cbe744d4d2194e76c86f7ac1bf8
Verdi.K <verdi@victron-tech.com>
2018-05-06 23:06:55 -0500

    Create user migration

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -3,22 +3,22 @@
 const Schema = use('Schema')
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', table => {
-      table.increments()
-      table.string('firstname').notNullable()
-      table.string('lastname').notNullable()
-      table.string('email').notNullable().unique()
-      table.string('password').notNullable()
-      table.string('number').notNullable().unique()
-      table.boolean('number_verified').defaultTo(false)
-      table.timestamps(true, true)
-    })
-  }
+    up () {
+        this.create('users', table => {
+            table.increments()
+            table.string('firstname').nullable()
+            table.string('lastname').nullable()
+            table.string('email').notNullable().unique()
+            table.string('password').notNullable()
+            table.string('number').notNullable().unique()
+            table.boolean('number_verified').defaultTo(false)
+            table.timestamps(true, true)
+        })
+    }
 
-  down () {
-    this.drop('users')
-  }
+    down () {
+        this.drop('users')
+    }
 }
 
 module.exports = UserSchema
