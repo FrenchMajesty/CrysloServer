23425b7d53f0995823965d871881d0a68f7d9c47
Verdi.K <verdi@victron-tech.com>
2018-05-09 13:02:31 -0500

    Add dates casting and formatting to User and WeCareContact model
    Since deleted_at is automatically left out of the lot I wrote this up to include it. I tried to add it to the SoftDelete trait but without success.

--- app/Models/User.js
+++ app/Models/User.js
@@ -26,6 +26,24 @@ class User extends Model {
     }
 
     /**
+     * Add columns to be casted and treated as dates
+     * @return {Array} 
+     */
+    static get dates() {
+        return super.dates.concat(['deleted_at'])
+    }
+
+     /**
+     * Format the date to be returned by the API
+     * @param  {String} field Name of the field
+     * @param  {Object} value moment.js instance of the date
+     * @return {String}       
+     */
+    static castDates(field, value) {
+        return `${value.fromNow(true)} ago`
+    }
+
+    /**
      * Get the rank associated with this user
      * @return {Rank} 
      */
--- app/Models/WeCareContact.js
+++ app/Models/WeCareContact.js
@@ -8,6 +8,24 @@ class WeCareContact extends Model {
     	this.addTrait('SoftDelete')
   	}
 
+    /**
+     * Add columns to be casted and treated as dates
+     * @return {Array} 
+     */
+    static get dates() {
+        return super.dates.concat(['deleted_at'])
+    }
+
+    /**
+     * Format the date to be returned by the API
+     * @param  {String} field Name of the field
+     * @param  {Object} value moment.js instance of the date
+     * @return {String}       
+     */
+    static castDates(field, value) {
+        return `${value.fromNow(true)} ago`
+    }
+
   	/**
   	 * Get the user who has this contact as one of their WeCare emergencies
   	 * @return {Array} 
