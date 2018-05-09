112730444a2dd525bb9a6cfb0c65514ecd5e8da6
Verdi.K <verdi@victron-tech.com>
2018-05-09 00:21:50 -0500

    Add `purpose` and `deleted_at` column in verification_codes table
    I've added the deleted_at so I can do soft deletes on the table rows and I added purpose because I looked at my written list of requirements and I realized that I needed to be able to know the purpose of the verification code for data & stats tracking.

--- database/migrations/1525671108167_verification_code_schema.js
+++ database/migrations/1525671108167_verification_code_schema.js
@@ -3,18 +3,20 @@
 const Schema = use('Schema')
 
 class VerificationCodeSchema extends Schema {
-  up () {
-    this.create('verification_codes', (table) => {
-      table.increments()
-      table.integer('number').notNullable()
-      table.string('code', 15).notNullable()
-      table.timestamps()
-    })
-  }
+    up () {
+        this.create('verification_codes', (table) => {
+            table.increments()
+            table.integer('number').notNullable()
+            table.string('code', 15).notNullable()
+            table.string('purpose').nullable()
+            table.timestamps(true, true)
+            table.timestamp('deleted_at').nullable()
+        })
+    }
 
-  down () {
-    this.drop('verification_codes')
-  }
+    down () {
+        this.drop('verification_codes')
+    }
 }
 
 module.exports = VerificationCodeSchema
