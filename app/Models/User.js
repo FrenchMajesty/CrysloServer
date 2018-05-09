90821cca7742c28f1517e1bffeaa3c412618f92d
Verdi.K <verdi@victron-tech.com>
2018-05-09 02:31:14 -0500

    Add relationship to User for Rank model
    Users have ranks and ranks can have many users with them.

--- app/Models/User.js
+++ app/Models/User.js
@@ -26,6 +26,14 @@ class User extends Model {
     }
 
     /**
+     * Get the rank associated with this user
+     * @return {Rank} 
+     */
+    rank() {
+        return this.belongsTo('App/Models/Rank')
+    }
+
+    /**
      * A relationship on tokens is required for auth to
      * work. Since features like `refreshTokens` or
      * `rememberToken` will be saved inside the
