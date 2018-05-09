4100e8d459d9f1eef8395ea7cc7698f65f2d5607
Verdi.K <verdi@victron-tech.com>
2018-05-09 01:49:54 -0500

    Add SoftDelete trait and create generateCode method on VerificationCode Lucid model
    I've added the SoftDelete trait because I want to be able to call .discard() on my verifications code and easily mark them as deleted in the database so I can know when they are used and not used.
    
    The generateCode() method is like the name suggest it, to generate a random 5 digit code to be assigned as the verification code for that entry.

--- app/Models/VerificationCode.js
+++ app/Models/VerificationCode.js
@@ -3,6 +3,18 @@
 const Model = use('Model')
 
 class VerificationCode extends Model {
+	static boot () {
+    	super.boot()
+    	this.addTrait('SoftDelete')
+  	}
+
+	/**
+	 * Create a random 5 digit code to be used as the verification code
+	 * @return {Void} 
+	 */
+	generateCode() {
+		this.code = Math.floor(10000 + Math.random() * 90000);
+	}
 }
 
 module.exports = VerificationCode
