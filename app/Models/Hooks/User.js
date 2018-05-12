b5411be9fa7e3caebed4f6f774960ab328e47604
Verdi.K <verdi@victron-tech.com>
2018-05-11 20:04:57 -0500

    Add `referralCode` hash method to User model
    So that the referral_id can be automatically generated upon the creation of a new user.

--- app/Models/Hooks/User.js
+++ app/Models/Hooks/User.js
@@ -1,20 +1,34 @@
 'use strict'
 
 const Hash = use('Hash')
+const crypto = require('crypto')
 
 const UserHook = module.exports = {}
 
 /**
- * Hash using password as a hook.
+* Hash using password as a hook.
+*
+* @method
+*
+* @param  {Object} userInstance
+* @return {void}
+*/
+UserHook.hashPassword = async (userInstance) => {
+	if (userInstance.password) {
+	userInstance.password = await Hash.make(userInstance.password)
+	}
+}
+
+/**
+ * Create a random referral code
  *
  * @method
- *
- * @param  {Object} userInstance
- *
- * @return {void}
+ * 
+ * @param  {Object} userInstance 
+ * @return {Void}              
  */
-UserHook.hashPassword = async (userInstance) => {
-  if (userInstance.password) {
-    userInstance.password = await Hash.make(userInstance.password)
-  }
+UserHook.referralCode = async (userInstance) => {
+	if(!userInstance.referral_id) {
+		userInstance.referral_id = crypto.randomBytes(3).toString('hex').toUpperCase()
+	}
 }
--- app/Models/User.js
+++ app/Models/User.js
@@ -3,17 +3,12 @@
 const Model = use('Model')
 
 class User extends Model {
+	
 	static boot () {
 		super.boot()
 
-		/**
-		 * A hook to hash the user password before saving
-		 * it to the database.
-		 *
-		 * Look at `app/Models/Hooks/User.js` file to
-		 * check the hashPassword method
-		 */
 		this.addHook('beforeCreate', 'User.hashPassword')
+		this.addHook('beforeCreate', 'User.referralCode')
 		this.addTrait('SoftDelete')
 	}
 
