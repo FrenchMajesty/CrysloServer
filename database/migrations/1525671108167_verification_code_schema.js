bc27f5d6d158cca7d0ba08647c1a4cada4f53e17
Verdi.K <verdi@victron-tech.com>
2018-05-12 19:20:34 -0500

    Change `number` column type in verification_code table
    It should be a string not a number because in the future this will include international numbers and those start with +(code).

--- database/migrations/1525671108167_verification_code_schema.js
+++ database/migrations/1525671108167_verification_code_schema.js
@@ -6,7 +6,7 @@ class VerificationCodeSchema extends Schema {
     up () {
         this.create('verification_codes', (table) => {
             table.increments()
-            table.integer('number').notNullable()
+            table.string('number').notNullable()
             table.string('code', 15).notNullable()
             table.string('purpose').nullable()
             table.timestamps(true, true)
