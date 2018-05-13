5a1b6ccb0b73f1f4b0ebe929ca09bccff2102c4c
Verdi.K <verdi@victron-tech.com>
2018-05-13 15:43:58 -0500

    Add generateDefault method on Guardian model
    I need to have default settigns for the guardians settings and I thought about doing it on the migration so that the default values are directly on the table/database but I didn't because to modify it later would be annoying because I would have to touch/mess with the table schema.
    
    This is the cleanest option to easily tweak & modify.

--- app/Models/Guardian.js
+++ app/Models/Guardian.js
@@ -5,6 +5,19 @@ const Model = use('Model')
 class Guardian extends Model {
 
 	/**
+	 * Set the default Guardians settings for this entry
+	 * @return {Void} 
+	 */
+	generateDefault() {
+		this.notify_self = true;
+		this.notify_wecare = true;
+		this.heart_min = 60;
+		this.heart_max = 160;
+		this.breath_min = 10;
+		this.breath_max = 30;
+	}	
+
+	/**
 	 * Return the user to whom these Guardians settings are for
 	 * @return {User}
 	 */
